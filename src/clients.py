a8b04a805bbf7b2580542afffc6db7bdff5ba66a
Anurag Gulati <anuraggulati1902@gmail.com>
2025-10-12 11:55:57 +0530

    push to SQS

--- src/clients.py
+++ src/clients.py
@@ -2,6 +2,11 @@ import boto3
 from functools import lru_cache
 from .config import AWS_REGION_NAME
 
+
 @lru_cache(maxsize=1)
 def get_ssm_client():
     return boto3.client('ssm', region_name=AWS_REGION_NAME)
+
+@lru_cache(maxsize=1)
+def get_sqs_client():
+    return boto3.client('sqs', region_name=AWS_REGION_NAME, config=boto3.session.Config(retries={'max_attempts': 10}))
