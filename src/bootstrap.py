051d97437558a91fbdec4476e5462ef5e6707a2f
Anurag Gulati <anuraggulati@Anurags-MacBook-Air.local>
2025-10-26 12:59:41 +0530

    exception management

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,4 +1,4 @@
-import orjson
+import json
 import logging
 from typing import Any
 
@@ -8,13 +8,17 @@ from .utils.clients import get_sqs_client # type: ignore
 
 def push_to_sqs(message: dict[str, Any]) -> None:
     try:
+        if not SQS_QUEUE_URL:
+            raise ValueError("SQS_QUEUE_URL not set")
         sqs_client = get_sqs_client()   # type: ignore
         response: dict[str, Any] = sqs_client.send_message( # type: ignore
             QueueUrl=SQS_QUEUE_URL,
-            MessageBody=orjson.dumps(message)
+            MessageBody=json.dumps(message)
         )
         logging.info(f"SQS message sent with ID: {response["MessageId"]}")
     except KeyError as e:
         logging.error(f"KeyError while pushing to SQS: {e}")
+        raise e
     except Exception as e:
         logging.error(f"Unexpected error while pushing to SQS: {e}")
+        raise e
